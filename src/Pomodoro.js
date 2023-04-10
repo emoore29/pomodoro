d9e5b960e41b7d23c0c937c0bc403a10182df5f1
AthenasCode <emoore5203@gmail.com>
2023-04-10 19:18:04 +0800

    finsihed responsive circle, tidied some code

--- src/Pomodoro.js
+++ src/Pomodoro.js
@@ -1,5 +1,5 @@
 import "./Pomodoro.css";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Timer from "./Components/Timer";
 import Menu from "./Components/Menu";
 
@@ -11,19 +11,19 @@ function Pomodoro() {
   const [rest, setRest] = useState(5);
   const [minutes, setMinutes] = useState(sessionMinutes); // starts with a session, not a rest
   const [seconds, setSeconds] = useState(0);
-
   const [focusColor, setFocusColor] = useState("#58427c"); // blue: 1a75a2, purple: 58427c
   const [restColor, setRestColor] = useState("#405E40");
   const [radius, setRadius] = useState(100);
+  const containerRef = useRef(null);
 
-  // Responsive clock radius
+  // Clock radius is responsive to window resizing
   useEffect(() => {
-    console.log("UseEffect ran");
     function handleResize() {
-      console.log("handling resize");
-      const clockContainerWidth =
-        document.getElementById("clock-container").offsetWidth;
-      setRadius(0.5 * clockContainerWidth);
+      if (containerRef.current) {
+        const clockContainerWidth = containerRef.current.offsetWidth;
+        console.log(clockContainerWidth);
+        setRadius(0.4 * clockContainerWidth);
+      }
     }
 
     window.addEventListener("resize", handleResize);
@@ -33,28 +33,41 @@ function Pomodoro() {
     };
   }, []);
 
-  const circumference = 2 * Math.PI * radius;
-  const timeLeft = minutes * 60 + seconds;
-  let totalTime;
-  typeOfSession === "session"
-    ? (totalTime = sessionMinutes * 60)
-    : (totalTime = rest * 60);
-  const strokeDashoffset = (1 - timeLeft / totalTime) * circumference; // calculates percentage of time left, offsets dasharray by that amount.
+  // Dimensions of circle calculations
+  function circleSvgCalcs() {
+    const circumference = 2 * Math.PI * radius;
+    const timeLeft = minutes * 60 + seconds;
+    let totalTime;
+    typeOfSession === "session"
+      ? (totalTime = sessionMinutes * 60)
+      : (totalTime = rest * 60);
+    const strokeDashoffset = (1 - timeLeft / totalTime) * circumference; // calculates percentage of time left, offsets dasharray by that amount.
 
-  const viewBoxSizing = radius * 2 + 10; //"-5 0 150 150"
-  const viewBox =
-    "-5 5 " + viewBoxSizing.toString() + " " + viewBoxSizing.toString();
-
-  // Bell ring
-  const beep = new Audio(
-    "https://actions.google.com/sounds/v1/alarms/phone_alerts_and_rings.ogg#t=33,38"
-  );
-
-  if (minutes === 0 && seconds === 0 && !mute) {
-    beep.play();
+    const viewBoxSizing = radius * 2 + 10; //"-5 0 150 150"
+    const viewBox =
+      "-5 5 " + viewBoxSizing.toString() + " " + viewBoxSizing.toString();
+    return {
+      strokeDashoffset,
+      viewBox,
+      circumference,
+    };
   }
 
-  // Other
+  const { strokeDashoffset, viewBox, circumference } = circleSvgCalcs();
+
+  // Bell rings at 00:00
+  function bellRing() {
+    const beep = new Audio(
+      "https://actions.google.com/sounds/v1/alarms/phone_alerts_and_rings.ogg#t=33,38"
+    );
+
+    if (minutes === 0 && seconds === 0 && !mute) {
+      beep.play();
+    }
+  }
+  bellRing();
+
+  // Toggles session type
   function toggleSessionType() {
     setSeconds(0);
     if (typeOfSession === "session") {
@@ -64,6 +77,7 @@ function Pomodoro() {
     }
   }
 
+  // Changes minutes displayed in timer depending on session type
   function updateDisplayOfMinutes() {
     if (!play) {
       //can only update minutes if session is paused
@@ -75,7 +89,7 @@ function Pomodoro() {
     }
   }
 
-  // this will run after every render.
+  // UseEffect handles clock interval and cancelling sessions
   useEffect(() => {
     let isCancelled = false; // updates only if user doesn't reset or pause midway through
     if (play && !isCancelled) {
@@ -108,14 +122,17 @@ function Pomodoro() {
     return () => clearInterval(interval);
   });
 
+  // Runs function to change display of minutes if minutes are updated, or type of session changes.
   useEffect(() => {
     updateDisplayOfMinutes();
   }, [sessionMinutes, rest, typeOfSession]);
 
+  // Updates background color for focus session
   function handleFocusColorChange(event) {
     setFocusColor(event.target.value);
   }
 
+  // Updates background color for break session
   function handleRestColorChange(event) {
     setRestColor(event.target.value);
   }
@@ -150,13 +167,8 @@ function Pomodoro() {
           color: "rgba(255, 255, 255, 0.5)",
         }}
       >
-        <div className="clock-container">
-          <svg
-            id="circle-svg"
-            viewBox={viewBox}
-            width={3 * radius}
-            height={3 * radius}
-          >
+        <div className="clock-container" ref={containerRef}>
+          <svg id="circle-svg" viewBox={viewBox}>
             <circle
               className="timer-progress"
               cx={radius}
